1bf253ee9d3183e5643e7d6dc31e0d6cf2773379
Bharti Swami <bhartiswamitech@gmail.com>
2020-07-09 02:46:08 +0530

    Taking inputs through Scanner Class in java

---
+++ javaIsFun/src/javaIsFun/ScannerImplementation.java
@@ -0,0 +1,20 @@
+package javaIsFun;
+import java.util.*;
+
+public class ScannerImplementation {
+	public static void main(String[] arg) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter an Integer");
+		int a=sc.nextInt();
+		System.out.println("a="+" "+a);
+		System.out.println("Enter a Double value");
+		Double b=sc.nextDouble();
+		System.out.println("b="+" "+b);
+		System.out.println("Enter a Bool value");
+		Boolean c=sc.nextBoolean();
+	    System.out.println("c="+" "+c);
+	    System.out.println("Enter a String");
+		String d = sc.nextLine(); 
+		System.out.println("d="+" "+d);
+	}
+}
