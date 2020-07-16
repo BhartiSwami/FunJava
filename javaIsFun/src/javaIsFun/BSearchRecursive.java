5bb6b7119559b68028817992eb49fb25b438bc87
Bharti Swami <bhartiswamitech@gmail.com>
2020-07-17 00:50:55 +0530

    Binary Search in array using recursion in java

--- javaIsFun/src/javaIsFun/BSearchRecursive.java
+++ javaIsFun/src/javaIsFun/BSearchRecursive.java
@@ -11,32 +11,33 @@ public class BSearchRecursive {
 			for(int i=0;i<n;i++) {
 				arr[i]=sc.nextInt();
 			}
-			int k=sc.nextInt();
-			int start=0;
-			int end=n-1;
-			BSearch(arr,k,start,end);
+			
+			BSearch(arr,n);
 		}
 	}
-	static void BSearch(int arr[], int k,int start,int end) {
-		int found=0;
-		while(start<=end) {
+	static void BSearch(int arr[], int n) {
+		int k=sc.nextInt();
+		int start=0;
+		int end=n-1;
+		if(BSAlgo(arr,k,start,end)) 
+			System.out.println("Element Found!");
+		else
+			System.out.println("Element NOT Found!");
+		
+	}
+	static boolean BSAlgo(int arr[],int k,int start,int end) {
+		while(start<end) {
 		int mid=(start+end)/2;
 		if(arr[mid]==k) {
-			found=1;
-			System.out.println("Element Found!");
-			return;
-			}
-		else if(arr[mid]>k)
-		{
-			end=mid-1;
+			return true;
 		}
-		else {
+		else if(k>arr[mid]) {
 			start=mid+1;
 		}
-		}
-		if(found==0) {
-			System.out.println("Element NOT Found");
+		else {
+			end=mid-1;
 		}
 	}
-	
+		return false;
+	}
 }
