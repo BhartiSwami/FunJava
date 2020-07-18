2a31c799e29b2ad8a6d11b939cc9b64aa9c1ebae
Bharti Swami <bhartiswamitech@gmail.com>
2020-07-19 00:23:43 +0530

    Insertion at head in Linked List

---
+++ javaIsFun/src/javaIsFun/LLHeadInsertion.java
@@ -0,0 +1,91 @@
+package javaIsFun;
+
+class Node{
+	int data;
+	Node next;
+	Node(int data){
+		this.data=data;
+		this.next=null;
+	}
+}
+
+public class LLHeadInsertion {
+	public static void main(String[] args) {
+		Node head=new Node(1);
+		head.next=new Node(2);
+		head.next.next=new Node(3);
+		head.next.next.next=new Node(4);
+		head.next.next.next.next=new Node(5);
+		head=insert(head);
+		print(head);
+	}
+	static Node insert(Node head) {
+		Node newNode=new Node(6);
+		newNode.next=head;
+		head=newNode;
+		return head;
+	}
+	static void print(Node head) {
+		Node temp=head;
+		while(temp!=null) {
+			System.out.print(temp.data+" ");
+			temp=temp.next;
+		}
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+
+	}
+}
