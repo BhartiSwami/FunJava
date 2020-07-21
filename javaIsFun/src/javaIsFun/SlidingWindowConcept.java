9f1a3a03264485782ad304a855be65cff6ad4167
Bharti Swami <bhartiswamitech@gmail.com>
2020-07-22 03:04:42 +0530

    Sliding window Concept for finding maximum sum of subarray of size k

--- javaIsFun/src/javaIsFun/SlidingWindowConcept.java
+++ javaIsFun/src/javaIsFun/SlidingWindowConcept.java
@@ -1,5 +1,31 @@
 package javaIsFun;
+import java.util.*;
+
+//CODE FOR FINDING MAXIMUM SUM OF SUBARRAY OK SIZE K
 
 public class SlidingWindowConcept {
-
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0)
+		{
+			int n=sc.nextInt();
+			int k=sc.nextInt();
+			int arr[]=new int[n];
+			for(int i=0;i<n;i++) {
+				arr[i]=sc.nextInt();
+			}
+			int sum=0;
+			for(int i=0;i<k;i++) {
+				sum=sum+arr[i];
+			}
+			int max=sum;
+			for(int i=0;i<n-k;i++) {
+				sum=sum-arr[i]+arr[i+k];
+				if(sum>max)
+					max=sum;
+			}
+			System.out.println(max);
+		}
+	}
 }
